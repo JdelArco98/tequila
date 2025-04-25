dc1138e80c722b6c35761f8499b58db24f983038
JdelArco98 <francisco.del.arco.santos@uni-a.de>
2025-04-25 12:11:41 +0200

    Update simulator_qulacs_gpu.py

--- src/tequila/simulators/simulator_qulacs_gpu.py
+++ src/tequila/simulators/simulator_qulacs_gpu.py
@@ -1,19 +1,42 @@
-import qulacs
-from qulacs_core import QuantumStateGpu
+# import qulacs
+# from qulacs_core import QuantumStateGpu
+#
+# from tequila import TequilaException
+# from tequila.simulators.simulator_qulacs import BackendCircuitQulacs, BackendExpectationValueQulacs
+#
+#
+# class TequilaQulacsGpuException(TequilaException):
+#     def __str__(self):
+#         return "Error in qulacs gpu backend:" + self.message
+#
+#
+# class BackendCircuitQulacsGpu(BackendCircuitQulacs):
+#     quantum_state_class = QuantumStateGpu
+#
+#
+# class BackendExpectationValueQulacsGpu(BackendExpectationValueQulacs):
+#     BackendCircuitType = BackendCircuitQulacsGpu
+#     pass
 
-from tequila import TequilaException
-from tequila.simulators.simulator_qulacs import BackendCircuitQulacs, BackendExpectationValueQulacs
+from tequila.simulators.simulator_base import BackendExpectationValue
+
+class FQE(BackendExpectationValue):
+    ExpValueType = None
+
+    def __init__(self, expval, *args, **kwargs):
+        self.HH = kwargs["FH"]
+        self.wfn = kwargs["wfn"]
+        self.UU = kwargs["FU"]
+        self.vv = kwargs["vnames"]
 
 
-class TequilaQulacsGpuException(TequilaException):
-    def __str__(self):
-        return "Error in qulacs gpu backend:" + self.message
+    def __call__(self, variables, *args, **kwargs):
+        variables = [variables[v] for v in self.vv]
+        wfn = self.wfn
+        for i, g in enumerate(self.UU):
+            wfn = wfn.time_evolve(-0.5 * variables[i], g)
+        energy = wfn.expectationValue(self.HH)
+        return energy
 
-
-class BackendCircuitQulacsGpu(BackendCircuitQulacs):
-    quantum_state_class = QuantumStateGpu
-
-
-class BackendExpectationValueQulacsGpu(BackendExpectationValueQulacs):
-    BackendCircuitType = BackendCircuitQulacsGpu
-    pass
+    def extract_variables(self):
+        return self.vv
